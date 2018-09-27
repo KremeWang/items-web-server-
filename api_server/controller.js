7d4a73836b7211c7b29f06cf7560a6efad1c9a81
kreme <1401005429@qq.com>
2018-09-27 12:29:29 +0800

    抽离业务逻辑处理模块优化代码

---
+++ api_server/controller.js
@@ -0,0 +1,121 @@
+//封装业务逻辑处理模块
+const mysql = require('mysql');
+//连接数据库
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'hero'
+})
+
+module.exports = {
+    testAPI: (req, res) => {
+        res.send('请求后台API接口成功！');
+    },
+    getAllHero: (req, res) => {
+        const sql = 'select * from heros';
+        conn.query(sql, (err, result) => {
+            if (err) return res.send({
+                status: 500,
+                msg: err.message,
+                data: null
+            })
+            res.send({
+                status: 200,
+                msg: '获取成功！',
+                data: result
+            })
+        })
+    },
+    addHero: (req, res) => {
+        //获取客户端提交来的英雄名称、性别
+        //获取服务器当前时间
+        // console.log(req.body);
+        const hero = req.body;
+        // 获取当前时间对象
+        const dt = new Date();
+        //padStart
+        const year = dt.getFullYear();
+        const month = (dt.getMonth() + 1).toString().padStart(2, '0');
+        const day = dt.getDate().toString().padStart(2, '0');
+        const hour = dt.getHours().toString().padStart(2, '0');
+        const minute = dt.getMinutes().toString().padStart(2, '0');
+        const second = dt.getSeconds().toString().padStart(2, '0');
+
+        hero.ctime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+        // console.log(hero);
+        const sql = 'insert into heros set ?';
+        conn.query(sql, hero, (err, result) => {
+            if (err) return res.send({
+                status: 500,
+                msg: err.message,
+                data: null
+            })
+            res.send({
+                status: 200,
+                msg: 'ok',
+                data: null
+            })
+        })
+        // res.send('ok');
+    },
+    getHeroById: (req, res) => {
+        const id = req.params.id;
+        const sql = 'select * from heros where id=?';
+        conn.query(sql, id, (err, result) => {
+            if (err) return res.send({
+                status: 500,
+                msg: err.message,
+                data: null
+            })
+            res.send({
+                status: 200,
+                msg: 'ok',
+                data: result
+            })
+        })
+    },
+    deleteHeroById: (req, res) => {
+        const id = req.params.id;
+        const sql = 'delete from heros where id=?';
+        conn.query(sql, id, (err, result) => {
+            if (err) return res.send({
+                status: 500,
+                msg: err.message,
+                data: null
+            })
+            res.send({
+                status: 200,
+                msg: 'ok',
+                data: null
+            })
+        })
+    },
+    updateHeroById: (req, res) => {
+        const id = req.params.id;
+        const newInfo = req.body;
+        const dt = new Date();
+        //padStart
+        const year = dt.getFullYear();
+        const month = (dt.getMonth() + 1).toString().padStart(2, '0');
+        const day = dt.getDate().toString().padStart(2, '0');
+        const hour = dt.getHours().toString().padStart(2, '0');
+        const minute = dt.getMinutes().toString().padStart(2, '0');
+        const second = dt.getSeconds().toString().padStart(2, '0');
+
+        newInfo.ctime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+        const sql = 'update heros set ? where id=?';
+        conn.query(sql, [newInfo, id], (err, result) => {
+            if (err) return res.send({
+                status: 500,
+                msg: err.message,
+                data: null
+            })
+            res.send({
+                status: 200,
+                msg: 'ok',
+                data: null
+            })
+        })
+    }
+}
--- api_server/router.js
+++ api_server/router.js
@@ -1,125 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const mysql = require('mysql');
-//连接数据库
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'hero'
-})
+const ctrl = require('./controller.js');
 
-router.get('/', (req, res) => {
-    res.send('请求后台API接口成功！');
-})
+router.get('/', ctrl.testAPI);
 
 //暴露getAllHero接口(获取全部英雄信息)
-router.get('/getAllHero', (req, res) => {
-    const sql = 'select * from heros';
-    conn.query(sql, (err, result) => {
-        if (err) return res.send({
-            status: 500,
-            msg: err.message,
-            data: null
-        })
-        res.send({
-            status: 200,
-            msg: '获取成功！',
-            data: result
-        })
-    })
-})
+router.get('/getAllHero',ctrl.getAllHero);
 
 //暴露添加英雄的API接口
-router.post('/addhero', (req, res) => {
-    //获取客户端提交来的英雄名称、性别
-    //获取服务器当前时间
-    // console.log(req.body);
-    const hero = req.body;
-    // 获取当前时间对象
-    const dt = new Date();
-    //padStart
-    const year = dt.getFullYear();
-    const month = (dt.getMonth() + 1).toString().padStart(2, '0');
-    const day = dt.getDate().toString().padStart(2, '0');
-    const hour = dt.getHours().toString().padStart(2, '0');
-    const minute = dt.getMinutes().toString().padStart(2, '0');
-    const second = dt.getSeconds().toString().padStart(2, '0');
-
-    hero.ctime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
-    // console.log(hero);
-    const sql = 'insert into heros set ?';
-    conn.query(sql, hero, (err, result) => {
-        if (err) return res.send({
-            status: 500,
-            msg: err.message,
-            data: null
-        })
-        res.send({
-            status: 200,
-            msg: 'ok',
-            data: null
-        })
-    })
-    // res.send('ok');
-})
+router.post('/addhero', ctrl.addHero);
 
 //根据id获取对应的英雄信息
-router.get('/gethero/:id', (req, res) => {
-    const id = req.params.id;
-    const sql = 'select * from heros where id=?';
-    conn.query(sql, id, (err, result) => {
-        if (err) return res.send({
-            status: 500,
-            msg: err.message,
-            data: null
-        })
-        res.send({
-            status: 200,
-            msg: 'ok',
-            data: result
-        })
-    })
-})
+router.get('/gethero/:id', ctrl.getHeroById);
 
 //根据ID更新对应的英雄信息
-router.post('/updatehero/:id', (req, res) => {
-    const id = req.params.id;
-    const newInfo = req.body;
-
-    console.log(newInfo);
-    const sql = 'update heros set ? where id=?';
-    conn.query(sql, [newInfo, id], (err, result) => {
-        if (err) return res.send({
-            status: 500,
-            msg: err.message,
-            data: null
-        })
-        res.send({
-            status: 200,
-            msg: 'ok',
-            data: null
-        })
-    })
-})
+router.post('/updatehero/:id', ctrl.updateHeroById);
 
 //根据ID信息删除对应英雄
-router.get('/deletehero/:id', (req, res) => {
-    const id = req.params.id;
-    const sql = 'delete from heros where id=?';
-    conn.query(sql, id, (err, result) => {
-        if (err) return res.send({
-            status: 500,
-            msg: err.message,
-            data: null
-        })
-        res.send({
-            status: 200,
-            msg: 'ok',
-            data: null
-        })
-    })
-})
+router.get('/deletehero/:id', ctrl.deleteHeroById);
 
 module.exports = router;
